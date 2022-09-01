eb8938a8af078a99c3892a13eda121e6397727fd
SUMITBOPATE <87616955+SUMITBOPATE@users.noreply.github.com>
2022-09-01 11:57:35 -0700

    Time: 1 ms (41.93%), Space: 42.3 MB (65.57%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       int lt = 0, i = 0, gt = nums.length - 1;
+        while (i <= gt) {
+            if (nums[i] == 0) {
+                swap(nums, lt++, i++);
+            } else if (nums[i] == 2) {
+                swap(nums, i, gt--);
+            } else { // nums[i] == 1
+                i++;
+            }
+        }
+    }
+    private void swap(int[] nums, int p1, int p2) {
+        int temp = nums[p1];
+        nums[p1] = nums[p2];
+        nums[p2] = temp;
+    }
+        
+        
+    
+}
