394c2dc589f876d60a9b9c653f581d5bed6f66e8
SUMITBOPATE <87616955+SUMITBOPATE@users.noreply.github.com>
2022-09-16 11:07:06 -0700

    Time: 1 ms (97.84%), Space: 42.1 MB (39.11%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+    HashMap<Character,String> map=new HashMap<>();
+       String word[]=s.split(" ");
+          if(pattern.length()!=word.length)
+        return false;
+    
+        for (int i=0;i<pattern.length();i++){
+           
+            char ch=pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(word[i]))
+                    return false;
+                  
+            }  else {
+                    if(map.containsValue(word[i]))
+                        return false;
+                    
+                    map.put(ch,word[i]);
+                     
+                
+            }
+            
+        }
+        return true;
+    }
+}
